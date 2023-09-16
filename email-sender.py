7757df49f467361173c73804873f413e836b7a6d
Justin Huynh <justinhuynh639@gmail.com>
2023-09-15 17:36:19 -0700

    completed sending email with python

--- email-sender.py
+++ email-sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Justin Huynh'
 email['to'] = 'justinhuynh855@gmail.com'
 email['subject'] = 'you won a millie'
 
-email.set_content('i am a boss')
+email.set_content(html.substitute({'name': 'Tintin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
